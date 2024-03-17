89732e7110f20d32e92cc051d36645d05ce0aa4d
Nilander Andrade <nilanderandrade@hotmail.com>
2024-03-17 12:00:32 -0300

    configurando area

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Browser, Routers, Route} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowseRouter>
+    <Routers>
+      <Route element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<Home />} />
+      <Route path="search/" element={<Home />} />
+      </Route>
+    </Routers>
+    </BrowseRouter>
   </React.StrictMode>,
 )
